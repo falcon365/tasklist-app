f756cff9424b1e851e0b3eab7f7a08311673d511
falcon <wv.falconman@gmail.com>
2020-05-09 09:48:28 +0000

    sighup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
     
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
     #root to: "tasks#index"
     root to: 'toppages#index'
+    
+    get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
     
 resources :tasks
 
